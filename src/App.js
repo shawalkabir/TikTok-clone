b0dbf50770df8c2a2f6201fb6458741f3a0d4185
Shawal Kabir Chy <shawalkabirchy2020@gmail.com>
2020-10-25 13:30:46 +0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Video from "./Video";
+import db from "./firebase";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    db.collection("videos").onSnapshot((snapshot) =>
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, [videos]);
+
+  return (
+    <div className="App">
+      <div className="app__videos">
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              messages={messages}
+              shares={shares}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
